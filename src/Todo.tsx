eebae1c8187a018d43f88fab5ecb556447e06265
ueferi <c5_nm@me.com>
2025-02-08 16:43:15 +0900

    Add edit functionality to TodoApp component

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -7,6 +7,12 @@ type Todo = {
 	detail: string;
 };
 
+type EditState = {
+	id: number | null;
+	title: string;
+	detail: string;
+};
+
 export const TodoApp: React.FC = () => {
 	const [todos, setTodos] = useState<Todo[]>([]);
 	const [title, setTitle] = useState("");
@@ -50,6 +56,40 @@ export const TodoApp: React.FC = () => {
 		);
 	});
 
+	// 編集機能のためのステート
+	const [editState, setEditState] = useState<EditState>({
+		id: null,
+		title: "",
+		detail: "",
+	});
+
+	// 編集の開始
+	const startEdit = (todo: Todo): void => {
+		setEditState({
+			id: todo.id,
+			title: todo.title,
+			detail: todo.detail,
+		});
+	};
+
+	// 編集フォームの入力更新
+	const handleEditChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	): void => {
+		setEditState({ ...editState, [e.target.name]: e.target.value });
+	};
+
+	// 編集の確定
+	const submitEdit = (): void => {
+		if (editState.id !== null) {
+			editTodo(editState.id, {
+				title: editState.title,
+				detail: editState.detail,
+			});
+			setEditState({ id: null, title: "", detail: "" });
+		}
+	};
+
 	return (
 		<div>
 			<h1>TODOリスト</h1>
@@ -80,6 +120,7 @@ export const TodoApp: React.FC = () => {
 					追加
 				</button>
 			</div>
+
 			<h2>フィルター</h2>
 			<div>
 				<input
@@ -108,6 +149,28 @@ export const TodoApp: React.FC = () => {
 					<option value="完了">完了</option>
 				</select>
 			</div>
+
+			{/* 編集フォーム：editState.id が null でなければ表示 */}
+			{editState.id !== null && (
+				<div>
+					<h2>編集フォーム</h2>
+					<input
+						type="text"
+						name="title"
+						value={editState.title}
+						onChange={handleEditChange}
+					/>
+					<textarea
+						name="detail"
+						value={editState.detail}
+						onChange={handleEditChange}
+					/>
+					<button type="button" onClick={submitEdit}>
+						更新
+					</button>
+				</div>
+			)}
+
 			<ul>
 				{filteredTodos.map((todo) => (
 					<li key={todo.id}>
@@ -122,6 +185,10 @@ export const TodoApp: React.FC = () => {
 							onClick={() => editTodo(todo.id, { status: "完了" })}
 						>
 							完了にする
+						</button>
+						{/* 編集開始ボタン */}
+						<button type="button" onClick={() => startEdit(todo)}>
+							編集
 						</button>
 					</li>
 				))}
